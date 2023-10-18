9e8c6f9340b1fe510a5296fda370f1797160573f
Radu3105 <radu.girlea@yahoo.com>
2023-10-19 00:27:48 +0300

    Add brush selection and paint functionality

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-const CANVAS_ROW_CELLS = 16;
-
 const canvas = document.querySelector('#canvas');
 const sizeSlider = document.querySelector('#size-slider');
+const modes = document.querySelector('#modes');
+const brushColor = document.querySelector('#brush-color');
+
+let selectedBrushMode = 'brush';
+let selectedBrushColor = 'black';
+let mouseDown = false;
+
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 function generateCanvas(size) {
     for (let i = 0; i < size; i++) {
@@ -12,6 +19,9 @@ function generateCanvas(size) {
             cell.setAttribute('class', 'cell');
             cell.style.cssText = `width: ${canvas.offsetWidth / size}px; height: ${canvas.offsetHeight / size}px`;
             row.appendChild(cell);
+            cell.addEventListener('mousedown', onMouseDown);
+            cell.addEventListener('mouseup', onMouseUp)
+            cell.addEventListener('mouseover', onMouseOver);
         }
         canvas.appendChild(row);
     }    
@@ -27,6 +37,56 @@ function updateCanvasSizeText() {
     const canvasSizeText = document.querySelector('#canvas-size');
     canvasSizeText.textContent = `Canvas Size: ${sizeSlider.value} x ${sizeSlider.value}`;
 }
+
+function onMouseDown(event) {
+    mouseDown = true;
+    event.target.style.backgroundColor = selectedBrushColor;
+    console.log(event.target);
+}
+
+function onMouseUp() {
+    mouseDown = false;
+}
+
+function onMouseOver(event) {
+    if (mouseDown) {
+        event.target.style.backgroundColor = selectedBrushColor;
+    }
+}
+
+function selectBrushMode() {
+    let currentActive = modes.querySelector('.active');
+    if (currentActive) currentActive.classList.remove('active');
+
+    switch (selectedBrushMode) {
+        case 'rainbow':
+            selectedBrushColor = 'pink';
+            break;
+        case 'eraser':
+            selectedBrushColor = 'white';
+            break;
+        case 'brush':
+            selectedBrushColor = brushColor.value;
+            break;
+    }
+}
+
+
+
+modes.addEventListener('click', (event) => {
+    if (event.target !== modes) {
+        selectedBrushMode = event.target.id;
+        selectBrushMode();
+        event.target.classList.add('active');
+    }
+});
+
+brushColor.addEventListener('change', () => {
+    selectedBrushColor = brushColor.value;
+    if (selectedBrushMode === 'brush') {
+        selectBrushMode();
+    }
+});
 
 generateCanvas(sizeSlider.value);
 
